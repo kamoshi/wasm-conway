d169be43c0f2b965cc8b71d43141aaa79dcd2b72
Maciej Jur <maciej@kamoshi.org>
2023-02-11 15:36:51 +0100

    Init repository

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import init from "rust-src";
+  import Board from "./Board.svelte";
+
+  const wasm = init();
+</script>
+
+<main>
+  {#await wasm}
+    Loading data...
+  {:then conway}
+    <Board {conway} />
+  {/await}
+</main>
---
+++ src/Board.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type {InitOutput} from "../rust-src/pkg";
+
+  export let conway: InitOutput;
+</script>
+
+
+<button on:click={conway.greet}>
+  Show alert
+</button>
